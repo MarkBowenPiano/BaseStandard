e18f4b90370864c95c5da59e3342158234272197
Ben Lumley <benbuildsstuff@gmail.com>
2013-11-09 11:47:01 +0000

    New Custom Query Loop for CPT

--- base-standard/custom_post_type.php
+++ base-standard/custom_post_type.php
@@ -8,21 +8,22 @@ Template Name: Custom Post Type Template
 
 	<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?><!-- Post Loop Breadcrumbs -->
 
-	<?php if (have_posts()) : ?>
-			
-    <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-	query_posts('post_type=*Custom_Post_Type*&posts_per_page=10&paged='. $paged ); ?>
-
-	<?php while (have_posts()) : the_post(); ?><!-- Start of the WordPress Custom Loop
-													Replace the *Custom_Post_Type* with the name of your Custom Post Type -->
+	<?php
+    $query_args = array(
+    'post_type' => '*Custom_Post_Type*',
+    'posts_per_page' => 3
+    );
+    $loop = new WP_Query($query_args);
+    while($loop->have_posts()) : $loop->the_post();
+	?>
+	<!-- Start of the WordPress Custom Loop
+	Replace the *Custom_Post_Type* with the name of your Custom Post Type -->
 	
 	<?php the_title() ;?><!-- Post Title -->
 
 	<?php the_excerpt(); ?><!-- Post Excerpt -->
 
     <?php endwhile; ?>
-	<?php else: ?>
-	<?php endif; ?><!-- end of WordPress loop -->
 
 	<?php get_sidebar(); ?><!-- WordPress Sidebar -->
 
